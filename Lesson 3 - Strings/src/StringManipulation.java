f8f0162c6978b29db9d8c96afb55de2c87a76b1b
kcoyle1 <kcoyle1@HS-D210-18.MOHS.local>
2019-02-25 13:23:28 -0500

    2/25/19

--- Lesson 3 - Strings/src/StringManipulation.java
+++ Lesson 3 - Strings/src/StringManipulation.java
@@ -4,13 +4,8 @@ public class StringManipulation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String myName = "Kevin Coyle";
-		int nameLength = myName.length();
-		System.out.println(nameLength - 1);
-		System.out.println(myName.substring(6));
-		System.out.println(myName.toUpperCase());
-		String lName = myName.substring(6);
-		String fName = myName.substring(0, 5);
-		System.out.println(fName + " " + lName);
+		String fName = myName.substring(2, myName.length());
+		System.out.println(fName);
 	}
 
 }
