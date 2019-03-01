414f2ef09fe7eee4fc72ae564412bce745233fdd
kcoyle1 <kcoyle1@HS-D210-18.MOHS.local>
2019-03-01 13:25:37 -0500

    3/1/19

--- Lesson 3 - Strings/src/Lesson3_2.java
+++ Lesson 3 - Strings/src/Lesson3_2.java
@@ -29,8 +29,15 @@ public class Lesson3_2 {
 		System.out.println(word5.substring(19, 20) + word5.substring(20, 36).toLowerCase() + "\t" + word5.substring(37, 44).toLowerCase() + " " +  word5.substring(0, 11) + "\t" + word5.substring(12, 18).toLowerCase());
 		System.out.println(" ");
 		String word6 = "word has 4 letters";
-		String partOfword6 = word6.substring(8);
-		System.out.println("\"" + word6.substring(9, 10) + "\"" + word6.substring(10, 18));
+		String partOfword6 = word6.substring(8, 9) + word6.substring(10, 18);
+		System.out.println("\"" + word6.substring(9, 10) + "\"" + word6.substring(10, 18) + word6.substring(4, 9) + "\"" + partOfword6.length() + "\"" + word6.substring(10, 18));
+		System.out.println(" ");
+		String word7 = "if SUBSTRING is used";
+		String partOfword7 = word7.substring(3, 12);
+		System.out.println(word7.substring(3, 13).toLowerCase() + word7.substring(13, 16) + partOfword7.length());
+		System.out.println(" ");
+		String word8 = "Jim, Jacky, Johnny, Jill";
+		System.out.println(word8.substring(0, 3).toLowerCase() + "\\3" + "\n");
 	}
 
 }
