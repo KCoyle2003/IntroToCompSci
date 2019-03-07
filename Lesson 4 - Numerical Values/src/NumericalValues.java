7ad897148f8691d7d166d31c5d7995dcd07f1804
kcoyle1 <kcoyle1@HS-D210-18.MOHS.local>
2019-03-07 13:23:28 -0500

    3/7/19

--- Lesson 4 - Numerical Values/src/NumericalValues.java
+++ Lesson 4 - Numerical Values/src/NumericalValues.java
@@ -5,8 +5,13 @@ public class NumericalValues {
 		// TODO Auto-generated method stub
 		System.out.println(5%3);
 		int ex1 = 11;
-		int ex2 = ex1;
+		int ex2 = ex1 + 3;
 		System.out.println(ex2);
+		int q = 5;
+		int r = 2 + q++;
+		System.out.println(r);
+		int s = q++ + 2;
+		System.out.println(s);
 	}
 
 }
