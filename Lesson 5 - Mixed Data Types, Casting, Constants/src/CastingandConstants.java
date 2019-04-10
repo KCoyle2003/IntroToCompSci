06148c15dcae113bcbbcce5ef8e276e0c634931a
kcoyle1 <kcoyle1@HS-D210-18.MOHS.local>
2019-04-10 13:21:37 -0400

    4/10/19

---
+++ Lesson 5 - Mixed Data Types, Casting, Constants/src/CastingandConstants.java
@@ -0,0 +1,9 @@
+//This program was created by Kevin Coyle on 4/10/19.
+public class CastingandConstants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
